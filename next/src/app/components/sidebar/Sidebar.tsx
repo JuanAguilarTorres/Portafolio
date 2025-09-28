48385be8223b844f6c7fa82677c2b216fe51d59a
Juan Aguilar <Juan.aguilartorres@ucr.ac.cr>
2025-09-27 22:59:25 -0600

    style(sidebar): add top margin for responsive layout

--- next/src/app/components/sidebar/Sidebar.tsx
+++ next/src/app/components/sidebar/Sidebar.tsx
@@ -16,7 +16,7 @@ export const Sidebar: React.FC<SidebarProps> = ({ className = '' }) => {
         <aside className={`bg-gradient-to-br from-slate-900 via-slate-800 to-slate-900 text-white 
                           h-auto lg:min-h-screen w-full lg:w-120 
                           flex flex-col ${className}`}>
-            <div className="flex flex-col justify-between h-full py-4 lg:py-8">
+            <div className="flex flex-col justify-between h-full py-4 lg:py-8 mt-6 lg:mt-0">
                 <div className="flex-1 flex flex-col justify-center px-4 lg:px-8">
                     <SidebarHeader />
                     <SidebarNav/>
