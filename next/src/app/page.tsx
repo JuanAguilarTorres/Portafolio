47ac75478869a560528551f99af53322f830e52d
Juan Aguilar <Juan.aguilartorres@ucr.ac.cr>
2025-09-28 01:58:20 -0600

    style(ui): adjust colors and page structure

--- next/src/app/page.tsx
+++ next/src/app/page.tsx
@@ -1,3 +1,4 @@
+// Index page
 import Image from "next/image";
 import { Roboto, Roboto_Mono } from "next/font/google";
 import "./globals.css";
@@ -15,21 +16,38 @@ const robotoMono = Roboto_Mono({
 });
 
 export default function Home() {
-  return (
-      <div className={`${roboto.variable} ${robotoMono.variable} grid items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 max-w-4xl mx-auto`}>
-          <div className="text-slate-400">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam commodo orci et odio iaculis,
-              in lacinia arcu elementum. Morbi quam libero, mattis non ipsum vitae, fermentum maximus quam.
-              Aliquam hendrerit augue ac porta malesuada. In suscipit risus ac metus pretium euismod. Nulla ac
-              efficitur augue, eu malesuada metus. Nam ac lorem in magna elementum porta. Praesent facilisis
-              sapien ac magna commodo, sed sodales leo auctor. In cursus eros non dolor placerat elementum.
-              Aenean in ornare risus. Integer nec venenatis enim, faucibus dignissim sapien. Proin nunc lectus,
-              malesuada placerat leo blandit, porttitor vestibulum ligula. Curabitur auctor pharetra tortor.
-          </div>
-          <h2>Projects</h2>
-          <div>content</div>
-          <h2>Contact</h2>
-          <div>content</div>
-    </div>
-  );
+    return (
+        <div className={`${roboto.variable} ${robotoMono.variable} grid items-center justify-items-center min-h-screen p-8 pb-20 gap-8 sm:p-20 max-w-4xl mx-auto`}>
+            <section className="space-y-6">
+                <p className="text-slate-400 leading-relaxed">
+                    Lorem ipsum <span className="font-bold text-white">dolor sit</span> amet, consectetur adipiscing elit. Etiam commodo orci et odio iaculis,
+                    in lacinia arcu <span className="font-bold text-white">elementum</span>. Morbi quam libero, mattis non ipsum vitae, fermentum maximus quam.
+                </p>
+                <p className="text-slate-400 leading-relaxed">
+                    Aliquam hendrerit <span className="font-bold text-white">augue ac</span> porta malesuada. In suscipit risus ac metus pretium euismod. Nulla ac
+                    efficitur augue, eu malesuada metus. Nam ac lorem in magna elementum porta. Praesent <span className="font-bold text-white">facilisis</span> ac
+                    sapien ac magna commodo, sed sodales leo auctor. In cursus eros non dolor placerat elementum.
+                </p>
+                <p className="text-slate-400 leading-relaxed">
+                    Aenean in <span className="font-bold text-white">ornare risus</span>. Integer nec venenatis enim, faucibus dignissim sapien. Proin nunc lectus,
+                    malesuada placerat leo blandit, <span className="font-bold text-white">porttitor vestibulum</span> ligula. Curabitur auctor pharetra tortor.
+                </p>
+            </section>
+
+            <section>
+                <h2 className="text-2xl font-bold mb-4">Projects</h2>
+                <div className="text-slate-400">
+                    Content
+                </div>
+            </section>
+
+            
+            <section>
+                <h2 className="text-2xl font-bold mb-4">Contact</h2>
+                <div className="text-slate-400">
+                    Content
+                </div>
+            </section>
+        </div>
+    );
 }
